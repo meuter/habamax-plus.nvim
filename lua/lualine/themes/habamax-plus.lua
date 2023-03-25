480ab1955d1ccc681839d5eb9e23975dc897f4ce
Cédric Meuter <cedric.meuter@gmail.com>
2023-03-25 11:16:51 +0100

    Patched the seol256 lualine theme to be a bit more readable

--- lua/lualine/themes/habamax-plus.lua
+++ lua/lualine/themes/habamax-plus.lua
@@ -4,38 +4,38 @@
 -- https://gist.github.com/shadmansaleh/000871c9a608a012721c6acc6d7a19b9
 -- stylua: ignore
 local colors = {
-    color0  = '#e8e8d3',
-    color1  = '#4e4e43',
-    color4  = '#30302c',
-    color5  = '#d7afaf',
-    color6  = '#666656',
-    color7  = '#808070',
-    color8  = 'AliceBlue',
-    color10 = '#87af87',
-    color13 = '#df5f87',
-    color14 = '#87afaf',
+    color0  = "#e8e8d3",
+    color1  = "#4e4e43",
+    color4  = "#30302c",
+    color5  = "#d7afaf",
+    color6  = "#666656",
+    color7  = "#808070",
+    color8  = "#c0c0ae",
+    color10 = "#87af87",
+    color13 = "#df5f87",
+    color14 = "#87afaf",
 }
 
 return {
     visual = {
-        a = { fg = colors.color4, bg = colors.color5, gui = 'bold' },
+        a = { fg = colors.color4, bg = colors.color5, gui = "bold" },
         b = { fg = colors.color0, bg = colors.color1 },
     },
     inactive = {
-        a = { fg = colors.color7, bg = colors.color4, gui = 'bold' },
+        a = { fg = colors.color7, bg = colors.color4, gui = "bold" },
         b = { fg = colors.color6, bg = colors.color4 },
         c = { fg = colors.color6, bg = colors.color4 },
     },
     insert = {
-        a = { fg = colors.color4, bg = colors.color10, gui = 'bold' },
+        a = { fg = colors.color4, bg = colors.color10, gui = "bold" },
         b = { fg = colors.color0, bg = colors.color1 },
     },
     replace = {
-        a = { fg = colors.color4, bg = colors.color13, gui = 'bold' },
+        a = { fg = colors.color4, bg = colors.color13, gui = "bold" },
         b = { fg = colors.color0, bg = colors.color1 },
     },
     normal = {
-        a = { fg = colors.color4, bg = colors.color14, gui = 'bold' },
+        a = { fg = colors.color4, bg = colors.color14, gui = "bold" },
         b = { fg = colors.color8, bg = colors.color1 },
         c = { fg = colors.color0, bg = colors.color4 },
     },
